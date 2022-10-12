8270d1af08c72a15ac4e47717cb67546e31540dc
Bo Chen <aaron027@Bos-MacBook-Air.gateway>
2022-10-13 08:40:56 +1100

    correct

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,34 +1,33 @@
+
 # Cloudfront distribution for main s3 site.
+
 resource "aws_cloudfront_distribution" "www_s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.www_bucket.website_endpoint
-    origin_id = "S3-www.${var.bucket_name}"
+    domain_name = aws_s3_bucket.www_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.www_bucket.bucket_regional_domain_name
 
-    custom_origin_config {
-      http_port = 80
-      https_port = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.cloudfront_access_identity_path
     }
   }
 
-  enabled = true
-  is_ipv6_enabled = true
+  enabled             = true
+  is_ipv6_enabled     = true
   default_root_object = "index.html"
 
   aliases = ["www.${var.domain_name}"]
 
   custom_error_response {
     error_caching_min_ttl = 0
-    error_code = 404
-    response_code = 200
-    response_page_path = "/404.html"
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/404.html"
   }
 
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id = "S3-www.${var.bucket_name}"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.www_bucket.bucket_regional_domain_name
 
     forwarded_values {
       query_string = false
@@ -39,11 +38,34 @@ resource "aws_cloudfront_distribution" "www_s3_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl = 31536000
-    default_ttl = 31536000
-    max_ttl = 31536000
-    compress = true
+    min_ttl                = 31536000
+    default_ttl            = 31536000
+    max_ttl                = 31536000
+    compress               = true
   }
+
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.www_bucket.bucket_regional_domain_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
 
   restrictions {
     geo_restriction {
@@ -52,36 +74,37 @@ resource "aws_cloudfront_distribution" "www_s3_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
-    ssl_support_method = "sni-only"
+    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
 
   tags = var.common_tags
+
+  depends_on = [
+    aws_s3_bucket.www_bucket
+  ]
 }
 
 # Cloudfront S3 for redirect to www.
 resource "aws_cloudfront_distribution" "root_s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.root_bucket.website_endpoint
-    origin_id = "S3-.${var.bucket_name}"
-    custom_origin_config {
-      http_port = 80
-      https_port = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    domain_name = aws_s3_bucket.root_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.root_bucket.bucket_regional_domain_name
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.cloudfront_access_identity_path
     }
   }
 
-  enabled = true
+  enabled         = true
   is_ipv6_enabled = true
 
   aliases = [var.domain_name]
 
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id = "S3-.${var.bucket_name}"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.root_bucket.bucket_regional_domain_name
 
     forwarded_values {
       query_string = true
@@ -94,11 +117,32 @@ resource "aws_cloudfront_distribution" "root_s3_distribution" {
     }
 
     viewer_protocol_policy = "allow-all"
-    min_ttl = 0
-    default_ttl = 86400
-    max_ttl = 31536000
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.root_bucket.bucket_regional_domain_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
   }
 
+  price_class = "PriceClass_200"
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -106,10 +150,122 @@ resource "aws_cloudfront_distribution" "root_s3_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
-    ssl_support_method = "sni-only"
+    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
 
   tags = var.common_tags
+
+  depends_on = [
+    aws_s3_bucket.root_bucket
+  ]
+}
+
+resource "aws_cloudfront_origin_access_identity" "cloudfront_origin_access_identity" {
+  comment    = "Only This User is allowed for S3 Read bucket"
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+resource "aws_s3_bucket_public_access_block" "root_bucket_public_access_block" {
+  bucket = aws_s3_bucket.root_bucket.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  depends_on              = [aws_cloudfront_distribution.root_s3_distribution]
+}
+
+resource "aws_s3_bucket_public_access_block" "www_bucket_public_access_block" {
+  bucket = aws_s3_bucket.www_bucket.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  depends_on              = [aws_cloudfront_distribution.www_s3_distribution]
+}
+
+data "aws_iam_policy_document" "root-cloudfront-read_bucket_only" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.root_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.root_bucket.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn]
+    }
+  }
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+data "aws_iam_policy_document" "www-cloudfront-read_bucket_only" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.www_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.www_bucket.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn]
+    }
+  }
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+resource "aws_s3_bucket_policy" "root_cloudfront_bucket" {
+  bucket     = aws_s3_bucket.root_bucket.id
+  policy     = data.aws_iam_policy_document.root-cloudfront-read_bucket_only.json
+  depends_on = [aws_cloudfront_distribution.root_s3_distribution]
+}
+resource "aws_s3_bucket_policy" "www_cloudfront_bucket" {
+  bucket     = aws_s3_bucket.www_bucket.id
+  policy     = data.aws_iam_policy_document.www-cloudfront-read_bucket_only.json
+  depends_on = [aws_cloudfront_distribution.www_s3_distribution]
+}
+
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on      = [null_resource.previous]
+  create_duration = "30s"
+}
+resource "time_sleep" "wait_60_seconds" {
+  depends_on      = [null_resource.previous]
+  create_duration = "60s"
+}
+
+output "www_regional_name" {
+  value = aws_s3_bucket.www_bucket.bucket_regional_domain_name
+}
+output "root_regional_name" {
+  value = aws_s3_bucket.root_bucket.bucket_regional_domain_name
+}
+
+output "identity-path"{
+  value = aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.cloudfront_access_identity_path
+}
+
+output "identity_arn"{
+  value =aws_cloudfront_origin_access_identity.cloudfront_origin_access_identity.iam_arn
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,7 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-  acl = "public-read"
+  acl    = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
 
   cors_rule {
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "www_bucket" {
 
   website {
     index_document = "index.html"
-    error_document = "404.html"
+    error_document = "error.html"
   }
 
   tags = var.common_tags
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "www_bucket" {
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-  acl = "public-read"
+  acl    = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
 
   website {
@@ -31,3 +31,6 @@ resource "aws_s3_bucket" "root_bucket" {
 
   tags = var.common_tags
 }
+
+
+
